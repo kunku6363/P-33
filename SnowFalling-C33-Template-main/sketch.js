b46fccaead3b94a2abe281bb567a5d06219dcb85
kunku6363 <78024774+kunku6363@users.noreply.github.com>
2021-06-02 15:47:54 +0530

    Add files via upload

---
+++ SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,18 @@
+var backgroundimg;
+
+
+
+function preload(){
+  backgroundimg = loadImage("snow3.jpg") ;
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(backgroundimg);  
+  drawSprites();
+}
